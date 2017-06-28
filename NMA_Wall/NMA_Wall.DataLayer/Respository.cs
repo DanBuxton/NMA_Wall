2ee0b4912eae3925c391a66641a867428c807a60
Andy Wyatt <andy.wyatt@bsdc.ac.uk>
2017-06-28 09:40:48 +0100

    GeoLocation methods moved out to a helper class.

--- NMA_Wall/NMA_Wall.DataLayer/Respository.cs
+++ NMA_Wall/NMA_Wall.DataLayer/Respository.cs
@@ -61,7 +61,7 @@ namespace NMA_Wall.DataLayer
         public IEnumerable<BO.Message> MessageGetInRange(double latitude, double longitude, double distance)
         {
             // TODO This is a really bad way of doing things... need to rethink this in the future
-            return DB.Messages.ToArray().Where(m => BO.Message.Distance(latitude, longitude, m.Latitude, m.Longitude) < distance);
+            return DB.Messages.ToArray().Where(m => BO.GeoLocationHelper.Distance(latitude, longitude, m.Latitude, m.Longitude) < distance);
         }
 
         public void ContentAdd(BO.Content content)
@@ -77,7 +77,7 @@ namespace NMA_Wall.DataLayer
         public IEnumerable<BO.Content> ContentGetInRange(double latitude, double longitude, double distance)
         {
             // TODO This is a really bad way of doing things... need to rethink this in the future
-            return DB.Content.ToArray().Where(c => BO.Message.Distance(latitude, longitude, c.Latitude, c.Longitude) < distance);
+            return DB.Content.ToArray().Where(c => BO.GeoLocationHelper.Distance(latitude, longitude, c.Latitude, c.Longitude) < distance);
         }
 
         public void SaveChanges()
