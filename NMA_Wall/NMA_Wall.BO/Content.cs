79429d39d08122bddae9c62d943f72fa3a7d14ed
30203038 <dan.buxton99@gmail.com>
2017-09-03 13:10:11 +0100

    Updated the Content class

--- NMA_Wall/NMA_Wall.BO/Content.cs
+++ NMA_Wall/NMA_Wall.BO/Content.cs
@@ -17,6 +17,17 @@ namespace NMA_Wall.BO
         public double Latitude { get; protected set; }
         public double Longitude { get; protected set; }
 
+        public Content(string mainHeading, string subjectHeading, string contentBody,
+            double latitude, double longitude) : this()
+        {
+            MainHeading = mainHeading;
+            SubjectHeading = subjectHeading;
+            ContentBody = contentBody;
+
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
         public Content()
         {
 
